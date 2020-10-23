00b50f886b728956b9f3399051dd998f091f8668
root <root@ubuntu.nxtsandbox.local>
2020-10-23 21:11:56 +0800

    comment

---
+++ src/test/java/com/hellokoding/account/model/MyTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+public class MyTest {
+ @Test
+ public void onePlusOneShouldBeTwo() {
+ int sum = 1 + 1;
+ assertEquals(2, sum);
+ }
+  
+  @Test
+  public void greetShouldIncludeTheOneBeingGreeted() {
+    String someone = "World";
+
+  }
+  @Test
+  public void greetBeingGreeted() {
+    String someone = "World";
+
+  }
+  @Test
+  public void ShouldIncludeTheOneBeingGreeted() {
+    String someone = "World";
+
+  }
+  @Test
+  public void greetIncludeTheOneBeingGreeted() {
+    String someone = "World";
+
+  }
+}
